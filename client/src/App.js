9c1b1ce9692bfb2621454c43ff8b6aacbbfdc5a3
amittras-pal <pal.amittras@gmail.com>
2021-11-03 15:42:58 +0530

    Add Toastify to Project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Slide, ToastContainer } from "react-toastify";
 import AppRouter from "./components/router/Router";
 
 function App() {
   return (
-    // <div className="App">
-    <Router>
-      <AppRouter />
-    </Router>
-    // </div>
+    <>
+      <Router>
+        <AppRouter />
+      </Router>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5500}
+        hideProgressBar={true}
+        newestOnTop={false}
+        transition={Slide}
+        rtl={false}
+        theme="colored"
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./resources/styles/theme.scss";
 import App from "./App";
 
