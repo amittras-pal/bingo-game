7c51cd4d4f5b351ba6ff277d7d15ad7b400e6039
amittras-pal <pal.amittras@gmail.com>
2021-11-05 18:29:20 +0530

    Conductor Layout and status update behaviour.

--- client/src/components/Game/Conductor/Conductor.jsx
+++ client/src/components/Game/Conductor/Conductor.jsx
@@ -6,32 +6,42 @@ import GameStats from "./GameStats/GameStats";
 
 function Conductor() {
   const { gameId, gameTitle } = JSON.parse(localStorage.getItem("gameData"));
-  const { gameData, startGame } = useConductorSocket();
+  const { gameData, startGame, generateNext } = useConductorSocket();
 
   return (
     <div className="container-fluid px-2 conductor scrollable">
       <div className="container my-3 p-0">
         <div className="row">
-          <div className="col-md-4 col-sm-12">
+          <div className="col-md-6 col-sm-12">
             <div className="generator-tile mb-3 p-3 text-center shadow glass-dark">
-              {gameData?.started || (
+              {gameData?.started ? (
+                <h6 className="text-light">Click button to get next number</h6>
+              ) : (
                 <h6 className="text-light">
                   Start the game to generate the first number
                 </h6>
               )}
               <div className="container text-center current text-light fw-bold">
-                XX
+                {gameData?.next ? <>{gameData.next}</> : <>XX</>}
               </div>
               <div className="conductor-actions mt-4">
-                <button
-                  className="btn btn-warning fw-bold shadow"
-                  onClick={() => startGame({ gameId, gameTitle })}>
-                  START GAME
-                </button>
+                {gameData?.started ? (
+                  <button
+                    className="btn btn-light fw-bold shadow"
+                    onClick={() => generateNext({ gameId, gameTitle })}>
+                    NEXT NUMBER
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-warning fw-bold shadow"
+                    onClick={() => startGame({ gameId, gameTitle })}>
+                    START GAME
+                  </button>
+                )}
               </div>
             </div>
           </div>
-          <div className="col-md-8 col-sm-12">
+          <div className="col-md-6 col-sm-12">
             <GameStats gameData={gameData} />
             <GameDetails gameData={gameData} />
           </div>
